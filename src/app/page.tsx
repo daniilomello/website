43c05865ff8e0a99bc813fb28aa0e05855faaf59
Danilo Mello <danilodemellow@gmail.com>
2024-06-28 00:04:01 -0300

    feat: add profile avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function Page() {
   return (
-    <main className="flex flex-col min-h-[100dvh] space-y-10">
-      <h1>Hello World</h1>
+    <main className="flex flex-col min-h-[100dvh] space-y-10 pt-10">
+      <section>
+        <div className="flex items-center gap-3">
+          <Avatar>
+            <AvatarImage src="https://github.com/daniilomello.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+          <h1 className="font-bold">Danilo Mello</h1>
+        </div>
+      </section>
     </main>
   );
 }
