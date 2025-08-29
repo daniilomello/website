a4baf1e92ebde6b0f3996b3f9a8ab988d45c8a80
Danilo Mello <danilodemellow@gmail.com>
2025-08-28 23:24:38 -0300

    feat: add new floating navigation menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,7 +110,7 @@ export default function RootLayout({
             <svg className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light" width="100%" height="100%"><filter id="noise"><feTurbulence type="fractalNoise" baseFrequency="0.80" numOctaves="4" stitchTiles="stitch"></feTurbulence></filter><rect width="100%" height="100%" filter="url(#noise)"></rect></svg>
             <ThemeProvider attribute="class" defaultTheme="dark">
               <TooltipProvider delayDuration={0}>
-                <main className="max-w-5xl mx-auto">
+                <main className="max-w-5xl mx-auto pt-24">
                   {children}
                   <CookiePopup />
                 </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,67 +1,41 @@
-import Meteors from "@/components/magicui/Meteors";
 import Link from "next/link";
 import { InteractiveHoverButton } from "./magicui/InteractiveHoverButton";
-import { SpinningText } from "./magicui/SpinningText";
-import { Story } from "./Stories";
-
-// TODO: update the component to be an floating bottom menu header for desktop and mobile
-const storiesData: Story[] = [
-    {
-        id: 1,
-        thumbnail: "https://github.com/daniilomello.png",
-        youtubeVideoId: "5OFVArgGgdI",
-    },
-];
+import Image from "next/image";
+import { Icons } from "./WhatsAppButton";
 
 export function Header() {
     return (
-        <header className="py-8 px-4 sm:px-0 sm:py-24 flex flex-col gap-4 sm:gap-0 sm:flex-row sm:justify-between sm:items-center">
-            <div className="flex items-center gap-9 justify-center sm:justify-normal relative -left-6 sm:-left-0">
-                <div className="relative">
-                    <span className="block w-[80px] h-[80px] relative left-6 -top-2">
-                        <div className="relative w-24 h-24">
-                            <div className="absolute inset-0 rounded-full animate-gradient-border bg-instagram-gradient" />
-                            <div className="absolute inset-0.5 rounded-full overflow-hidden">
-                                <div
-                                className="relative z-10 w-full h-full cursor-pointer shadow-md hover:shadow-lg transition-shadow"
-                                >
-                                    <img
-                                        src="https://github.com/daniilomello.png"
-                                        alt="profile picture"
-                                        className="object-cover w-full h-full rounded-full"
-                                    />
-                                </div> 
-                            </div>
-                        </div>
-                    </span>
-                    <SpinningText className="absolute top-10 left-[71px]  text-lg">
-                        Frontend ⚡ Backend ⚡ Mobile ⚡ UI & UX ⚡
-                    </SpinningText>
+        <header className="fixed bottom-4 inset-x-0 z-50 w-fit mx-auto px-3 p-2 sm:p-3 rounded-full border border-white/10 bg-black/20 backdrop-blur-lg">
+            <nav className="flex items-center gap-3 sm:gap-6 justify-center sm:justify-normal">
+                <Link href="/" className="relative">
+                    <Image src="https://github.com/daniilomello.png" alt="Danilo Mello" width={48} height={48} className="w-8 h-8 sm:w-12 sm:h-12 rounded-full border-2 border-pink-500 transition-all hover:scale-110 hover:brightness-110" />
+                </Link>
+                <div className="flex items-center gap-3 text-sm sm:text-base">
+                    <Link href="/" className="hover:text-rose-300 flex items-center gap-1 relative">
+                        Home
+                    </Link>
+                    <span>•</span>
+                    <Link href="/blog" className="hover:text-rose-300 flex items-center gap-1 relative">
+                        Posts
+                    </Link>
+                    <span>•</span>
+                    <Link href="/about" className="hover:text-rose-300 flex items-center gap-1 relative">
+                        About
+                    </Link>
+                    <span>•</span>
+                    <Link href="/resume.pdf" target="_blank" className="hover:text-rose-300">Resume</Link>
+                    <span className="hidden sm:inline-block">•</span>
+                    <Link href="https://wa.me/5534984424459" target="_blank" className="hidden sm:inline-block">
+                        <InteractiveHoverButton className="text-xs">
+                            Get in Touch
+                        </InteractiveHoverButton>
+                    </Link>
+                    <span className="sm:hidden">•</span>
+                    <Link href="https://wa.me/5534984424459" target="_blank" className="sm:hidden scale-75">
+                        <Icons.whatsapp />
+                    </Link>
                 </div>
-            </div>
-
-            <nav className="flex items-center gap-3 justify-center sm:justify-normal mt-10 sm:mt-0">
-                <Link href="/" className="hover:text-rose-300 flex items-center gap-1 relative">
-                    Home
-                </Link>
-                <span>•</span>
-                <Link href="/blog" className="hover:text-rose-300 flex items-center gap-1 relative">
-                    Posts
-                </Link>
-                <span>•</span>
-                <Link href="/about" className="hover:text-rose-300 flex items-center gap-1 relative">
-                    About
-                </Link>
-                <span>•</span>
-                <Link href="/resume.pdf" target="_blank" className="hover:text-rose-300">Resume</Link>
-                <span className="hidden sm:inline-block">•</span>
-                <Link href="https://wa.me/5534984424459" target="_blank" className="hidden sm:inline-block">
-                    <InteractiveHoverButton className="text-xs">
-                        Get in Touch
-                    </InteractiveHoverButton>
-                </Link>
             </nav>
-            <Meteors number={30} />
         </header>
     )
 }
